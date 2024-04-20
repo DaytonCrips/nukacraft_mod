6e6f54a3193a31612109b470e9baa993b4f1031b
Jetug <ksergeev01@gmail.com>
2024-04-21 01:26:38 +0300

    Fixed item tabs

--- src/main/java/com/nukateam/nukacraft/common/registery/ModItemTabs.java
+++ src/main/java/com/nukateam/nukacraft/common/registery/ModItemTabs.java
@@ -1,15 +1,14 @@
 package com.nukateam.nukacraft.common.registery;
 
-import com.nukateam.nukacraft.NukaCraftMod;
+import com.nukateam.nukacraft.*;
 import com.nukateam.nukacraft.common.registery.items.*;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.registries.*;
+import net.minecraft.network.chat.*;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.*;
+
+import static net.minecraft.world.item.ArmorItem.Type.*;
 
 public class ModItemTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
@@ -23,33 +22,33 @@ public class ModItemTabs {
 
 
     public static final RegistryObject<CreativeModeTab> NUKA_BLOCKS = CREATIVE_MODE_TABS.register("nuka_blocks",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ADVMAG.get()))
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlockItems.FIREFUNGI.get()))
                     .title(Component.translatable("itemGroup.nuka_blocks"))
                     .displayItems((params, output) -> registerItems(output, ModBlockItems.ITEMS))
                     .build());
 
     public static final RegistryObject<CreativeModeTab> NUKA_FOOD = CREATIVE_MODE_TABS.register("nuka_foods",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ADVMAG.get()))
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModFood.NUKACOLA.get()))
                     .title(Component.translatable("itemGroup.nuka_foods"))
                     .displayItems((params, output) -> registerItems(output, ModFood.ITEMS))
                     .build());
 
 
     public static final RegistryObject<CreativeModeTab> NUKA_WEAPONS = CREATIVE_MODE_TABS.register("nuka_equip",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ADVMAG.get()))
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModWeapons.ROUND10MM.get()))
                     .title(Component.translatable("itemGroup.nuka_equip"))
-                    .displayItems((params, output) -> registerItems(output, ModFood.ITEMS))
+                    .displayItems((params, output) -> registerItems(output, ModWeapons.ITEMS))
                     .build());
 
 
     public static final RegistryObject<CreativeModeTab> NUKA_ARMOR = CREATIVE_MODE_TABS.register("nuka_armor",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ADVMAG.get()))
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModArmorItems.METAL_SET.get(HELMET).get()))
                     .title(Component.translatable("itemGroup.nuka_armor"))
                     .displayItems((params, output) -> registerItems(output, ModArmorItems.ITEMS))
                     .build());
 
     public static final RegistryObject<CreativeModeTab> POWER_ARMOR = CREATIVE_MODE_TABS.register("power_armor",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ADVMAG.get()))
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(PowerArmorItems.T45_HELMET.get()))
                     .title(Component.translatable("itemGroup.power_armor"))
                     .displayItems((params, output) -> registerItems(output, PowerArmorItems.ITEMS))
                     .build());
@@ -76,15 +75,7 @@ public class ModItemTabs {
         } catch (Exception e) {
             NukaCraftMod.LOGGER.error(e.getMessage(), e);
         }
-
     }
-
-//    public static final CreativeModeTab NUKA_ARMOR = new CreativeModeTab("nuka_armor") {
-//        @Override
-//        public ItemStack makeIcon() {
-//            return new ItemStack(ModArmorItems.WOOD2_BODY.get());
-//        }
-//    };
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
