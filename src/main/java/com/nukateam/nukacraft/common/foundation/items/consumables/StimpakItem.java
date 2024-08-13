852fc2bee5246405a16634b058921047ea7cfde8
jetug <chizeelb@gmail.com>
2024-08-13 13:50:11 +0300

    Stimpak can be used on other entities

--- src/main/java/com/nukateam/nukacraft/common/foundation/items/consumables/StimpakItem.java
+++ src/main/java/com/nukateam/nukacraft/common/foundation/items/consumables/StimpakItem.java
@@ -1,16 +1,22 @@
 package com.nukateam.nukacraft.common.foundation.items.consumables;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.IForgeShearable;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Random;
 
 public class StimpakItem extends Item {
     private final int heal;
@@ -20,12 +26,25 @@ public class StimpakItem extends Item {
         this.heal = heal;
     }
 
+    public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity entity, InteractionHand hand) {
+        if (!entity.level().isClientSide && entity.getHealth() < entity.getMaxHealth()) {
+            entity.heal(heal);
+
+            if (!player.isCreative()) {
+                stack.shrink(1);
+            }
+        }
+        return InteractionResult.SUCCESS;
+    }
+
     @Override
     public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity entity) {
-        entity.heal(heal);
+        if(entity.getHealth() < entity.getMaxHealth()) {
+            entity.heal(heal);
 
-        if (!(entity instanceof Player player) || !player.isCreative()) {
-            stack.shrink(1);
+            if (!(entity instanceof Player player) || !player.isCreative()) {
+                stack.shrink(1);
+            }
         }
 
         return stack;
