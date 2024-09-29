d264fd9165b0d881f168f8679953515764de6751
Jetug <ksergeev01@gmail.com>
2024-09-29 22:04:00 +0300

    Brahmin balls code

--- src/main/java/com/nukateam/nukacraft/common/foundation/entities/mobs/Brahmin.java
+++ src/main/java/com/nukateam/nukacraft/common/foundation/entities/mobs/Brahmin.java
@@ -1,31 +1,48 @@
 package com.nukateam.nukacraft.common.foundation.entities.mobs;
 
+import com.nukateam.nukacraft.common.registery.items.ModFood;
 import mod.azure.azurelib.animatable.GeoEntity;
 import mod.azure.azurelib.core.animatable.instance.AnimatableInstanceCache;
 import mod.azure.azurelib.core.animation.AnimatableManager;
 import mod.azure.azurelib.core.animation.AnimationController;
-import mod.azure.azurelib.core.animation.RawAnimation;
 import mod.azure.azurelib.core.object.PlayState;
 import mod.azure.azurelib.util.AzureLibUtil;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.Shearable;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.animal.Cow;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraftforge.common.IForgeShearable;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.List;
 
 import static com.nukateam.nukacraft.common.registery.EntityTypes.BRAHMIN;
 import static mod.azure.azurelib.core.animation.RawAnimation.begin;
 
-public class Brahmin extends Cow implements GeoEntity {
+public class Brahmin extends Cow implements GeoEntity, Shearable, IForgeShearable {
     private final AnimatableInstanceCache cache = AzureLibUtil.createInstanceCache(this);
+
+    private static final EntityDataAccessor<Boolean> HAS_BALLS = SynchedEntityData.defineId(Brahmin.class, EntityDataSerializers.BOOLEAN);
 
     public Brahmin(EntityType<? extends Cow> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
@@ -48,6 +65,24 @@ public class Brahmin extends Cow implements GeoEntity {
         this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
     }
 
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        entityData.define(HAS_BALLS, true);
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag compound) {
+        super.addAdditionalSaveData(compound);
+        compound.putBoolean("hasBalls", this.hasBalls());
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag pCompound) {
+        super.readAdditionalSaveData(pCompound);
+        this.hasBalls(pCompound.getBoolean("hasBalls"));
+    }
+
     @Nullable
     @Override
     public Brahmin getBreedOffspring(ServerLevel level, AgeableMob otherParent) {
@@ -55,18 +90,57 @@ public class Brahmin extends Cow implements GeoEntity {
     }
 
     @Override
+    public boolean isShearable(@NotNull ItemStack item, Level level, BlockPos pos) {
+        return readyForShearing();
+    }
+
+    @Override
+    public boolean readyForShearing() {
+        return hasBalls();
+    }
+
+    @Override
+    public @NotNull List<ItemStack> onSheared(@Nullable Player player, @NotNull ItemStack item, Level level, BlockPos pos, int fortune) {
+        var soundSource = player == null ? SoundSource.BLOCKS : SoundSource.PLAYERS;
+        this.level().playSound(null, this, SoundEvents.SHEEP_SHEAR, soundSource, 1.0F, 1.0F);
+        this.gameEvent(GameEvent.SHEAR, player);
+
+        if(!level.isClientSide) {
+            var stack = new ItemStack(ModFood.BRAHMIN_TESTICULES.get(), 4);
+            var damageSource = player == null ? this.damageSources().generic() : this.damageSources().playerAttack(player);
+
+            this.hurt(damageSource, 1);
+            this.hasBalls(false);
+
+            return Collections.singletonList(stack);
+        }
+
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void shear(SoundSource pSource) {
+        this.level().playSound(null, this, SoundEvents.SHEEP_SHEAR, pSource, 1.0F, 1.0F);
+        this.hasBalls(false);
+
+        var stack = new ItemStack(ModFood.BRAHMIN_TESTICULES.get());
+
+        for(int j = 0; j < 4; ++j) {
+            spawnItemEntity(stack);
+        }
+    }
+
+    @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
         controllerRegistrar.add(new AnimationController<>(this, "controller", 0, event -> {
             var controller = event.getController();
             controller.setAnimationSpeed(1);
-            RawAnimation animation;
+            var animation = begin();
 
             if (event.isMoving()) {
-                animation = begin().thenLoop("walk");
-            } else {
-//                animation = begin().thenLoop("idle");
-                return PlayState.STOP;
+                animation.thenLoop("walk");
             }
+            else return PlayState.STOP;
 
             return event.setAndContinue(animation);
         }));
@@ -75,5 +149,24 @@ public class Brahmin extends Cow implements GeoEntity {
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return cache;
+    }
+
+    public boolean hasBalls() {
+        return this.entityData.get(HAS_BALLS);
+    }
+
+    public void hasBalls(boolean value) {
+        this.entityData.set(HAS_BALLS, value);
+    }
+
+    private void spawnItemEntity(ItemStack stack) {
+        var itemEntity = this.spawnAtLocation(stack);
+
+        if (itemEntity != null) {
+            itemEntity.setDeltaMovement(itemEntity.getDeltaMovement().add((
+                            this.random.nextFloat() - this.random.nextFloat()) * 0.1F,
+                    this.random.nextFloat() * 0.05F,
+                    (this.random.nextFloat() - this.random.nextFloat()) * 0.1F));
+        }
     }
 }
